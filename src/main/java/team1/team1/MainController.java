e9db4569aeed65da34396e346352f7b7b8c844d0
jihongeek <jihongeek@kakao.com>
2024-07-26 22:14:22 +0900

    Feat: JPA로 데이터베이스 정보를 가져오기 위한 컨트롤러 수정

--- src/main/java/team1/team1/MainController.java
+++ src/main/java/team1/team1/MainController.java
@@ -1,44 +1,24 @@
 package team1.team1;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.ResponseBody;
 
-import java.lang.reflect.Member;
+import java.util.List;
 
 
-class Content {
-    public int id;
-    public String type;
-    public String host;
-    public String date;
-    public String title;
-    public Content(int id, String type, String host, String date, String title) {
-        this.id = id;
-        this.type = type;
-        this.host = host;
-        this.date = date;
-        this.title = title;
-    }
-}
-class MemberInfo {
-    public int id;
-    public String name;
-    public  String phoneNumber;
 
-    public  MemberInfo(int id, String name, String phoneNumber)
-    {
-         this.id = id;
-         this.name = name;
-         this.phoneNumber = phoneNumber;
-    }
-
-}
 @Controller
 public class MainController {
+
+    @Autowired
+    private ProgramRepository programRepository;
+
+    @Autowired
+    private MemberInfoRepository memberInfoRepository;
     @GetMapping("/")
     public String mainPage(Model model) {
         return "index";
@@ -60,7 +40,7 @@ public class MainController {
     }
 
     @GetMapping("/workwrite")
-    public String jobWritePage() {
+    public String workWritePage() {
         return "workwrite";
     }
 
@@ -77,40 +57,28 @@ public class MainController {
 
     @GetMapping("/mypage_enrolled")
     public String myPageEnrolled(Model model) {
-        Content[] contents = {
-                new Content(0,"edu","동의대학교","2022년 8월 20일","영어회화 첫걸음"),
-                new Content(1,"job","BNK 금융그룹","2022년 8월 20일","앱 API 테스터 모집"),
-                new Content(2,"job","BNK 금융그룹","2022년 8월 20일","앱 API 테스터 모집"),
-                new Content(3,"edu","동의대학교","2022년 8월 20일","영어회화 첫걸음")
-        };
-        model.addAttribute("contents",contents);
+        List<Program> programs = programRepository.findAll();
+        model.addAttribute("programs", programs);
         return "mypage_enrolled";
     }
 
     @GetMapping("/mypage_created")
     public String myPageCreated(Model model) {
-        Content[] contents = {
-                new Content(0,"edu","동의대학교","2022년 8월 20일","영어회화 첫걸음"),
-                new Content(1,"job","BNK 금융그룹","2022년 8월 20일","앱 API 테스터 모집"),
-        };
-        model.addAttribute("contents",contents);
+        List<Program> programs = programRepository.findAll();
+        model.addAttribute("programs", programs);
         return "mypage_created";
     }
 
     @GetMapping("/applicant_list/{type}/{id}")
     public String myPageEnrolled(@PathVariable("type") String type, @PathVariable("id") int id,  Model model) {
-        Content[] contents = {
-                new Content(0,"edu","동의대학교","2022년 8월 20일","영어회화 첫걸음"),
-                new Content(1,"job","BNK 금융그룹","2022년 8월 20일","앱 API 테스터 모집"),
+        Program[] programs = {
+                new Program(0,"edu",1,"동의대학교","2022년 8월 20일","영어회화 첫걸음"),
+                new Program(1,"job",1,"BNK 금융그룹","2022년 8월 20일","앱 API 테스터 모집"),
         };
-        Content content = contents[id];
-        MemberInfo[] applicants = {
-                new MemberInfo(0,"김지홍","010-1234-1234"),
-                new MemberInfo(1,"김지홍","010-1234-1234")
-        };
-        model.addAttribute("content",content);
-        model.addAttribute("applicants",applicants);
+        Program program = programs[id];
+        memberInfoRepository.findAll();
+        model.addAttribute("content", program);
+        model.addAttribute("content", program);
         return "applicant_list";
     }
-
 }
