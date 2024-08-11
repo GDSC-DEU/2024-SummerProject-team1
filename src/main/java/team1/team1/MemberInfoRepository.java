16be1ee4c55982f2b61e66f410cc9f3d4b00fcc0
Jihong Kim <34394165+jihongeek@users.noreply.github.com>
2024-08-11 15:06:53 +0900

    최후의 커밋.

--- src/main/java/team1/team1/MemberInfoRepository.java
+++
@@ -1,6 +0,0 @@
-package team1.team1;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface MemberInfoRepository extends JpaRepository<MemberInfo, Integer> {
-    MemberInfo findByName(String userId);
-}
